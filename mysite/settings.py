987143959a741a46fa2759cb60db5c2867e67c26
leeskw <leeskw@nate.com>
2023-01-17 23:37:32 -0800

    Modified mysite/settings.py.

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,7 +118,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'STATIC'   # new
+STATIC_ROOT = BASE_DIR / 'static'   # new
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
